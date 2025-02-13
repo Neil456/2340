a42ca8863882941f833f38f8052f0a17b1dd6a81
Neil Mehta <neilmehta471@gmail.com>
2025-02-13 16:07:12 -0500

    Changed home page

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    movies = Movie.objects.all()  # Retrieve all movies from the database
+    movies = Movie.objects.order_by('-id')[:4] 
     template_data = {
         'title': 'GT Movies Store',
         'movies': movies  # Pass movies to the template
