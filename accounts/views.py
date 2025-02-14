80a86e193219ccaafa555c4befd48e3de0c6500a
shora051 <sahil.hora.26@gmail.com>
2025-02-14 15:41:24 -0500

    Added Order and Purchase

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import login as auth_login, authenticate, logout as aut
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 @login_required
 def logout(request):
     auth_logout(request)
